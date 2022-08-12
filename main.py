d4cf102a400e0a00ba51d80b1017bcb4100bf15a
Guillaume Faguet <guillaume.faguet@protonmail.com>
2022-08-12 15:24:33 +0200

    refactor of the solve function(callback)

--- main.py
+++ main.py
@@ -11,135 +11,79 @@ from kivy.uix.textinput import TextInput
 
 
 class OhmsLawInputScreen(GridLayout):
-
     def __init__(self, **kwargs):
         super(OhmsLawInputScreen, self).__init__(**kwargs)
         self.cols = 1
         self.rows = 10
-        title = Label(text='Ohm\'s Law Solver!', font_size='32sp', color=(1, .8, 0), bold=True, size_hint=(1, .5))
+        title = Label(
+            text="Ohm's Law Solver!",
+            font_size="32sp",
+            color=(1, 0.8, 0),
+            bold=True,
+            size_hint=(1, 0.5),
+        )
         self.add_widget(title)
-        instructions = Label(text='Enter Known Values (Minimum 2)', font_size='20sp', bold=True, size_hint=(1, .5))
+        instructions = Label(
+            text="Enter Known Values (Minimum 2)",
+            font_size="20sp",
+            bold=True,
+            size_hint=(1, 0.5),
+        )
         self.add_widget(instructions)
-        self.voltage = LabelAndInput('Voltage (V - volts): ', (1, 0, 0))
-        self.current = LabelAndInput('Current (I - amps): ', (0, 1, 0))
-        self.resistance = LabelAndInput('Resistance (R - ohms): ', (0, 0.5, 1))
-        self.power = LabelAndInput('Power (P - watts): ', (1, 0, 1))
+        self.voltage = LabelAndInput("Voltage (V - volts): ", (1, 0, 0))
+        self.current = LabelAndInput("Current (I - amps): ", (0, 1, 0))
+        self.resist = LabelAndInput("Resistance (R - ohms): ", (0, 0.5, 1))
+        self.power = LabelAndInput("Power (P - watts): ", (1, 0, 1))
         self.add_widget(self.voltage)
         self.add_widget(self.current)
-        self.add_widget(self.resistance)
+        self.add_widget(self.resist)
         self.add_widget(self.power)
-        self.buttons = ButtonBox()
-        self.buttons.solve_v.bind(on_press=partial(self.solve, 'v'))
-        self.buttons.solve_i.bind(on_press=partial(self.solve, 'i'))
-        self.buttons.solve_r.bind(on_press=partial(self.solve, 'r'))
-        self.buttons.solve_p.bind(on_press=partial(self.solve, 'p'))
-        self.add_widget(self.buttons)
-        self.answer = Label(text='Answer: ', bold=True, size_hint=(1, .5))
-        self.formula = Label(text='Formula: ', bold=True, size_hint=(1, .5))
+        self.solve = Button(text="Solve", color=(0, 0, 0), bold=True)
+        self.add_widget(self.solve)
+        self.solve.bind(on_press=self.callback)
+        self.answer = Label(text="Answer: ", bold=True, size_hint=(1, 0.5))
         self.add_widget(self.answer)
+        self.formula = Label(text="Formula: ", bold=True, size_hint=(1, 0.5))
         self.add_widget(self.formula)
 
-    def solve(self, target, button):
-        values = [0, 0, 0, 0]
-        if self.voltage.input.text != '':
-            values[0] = float(self.voltage.input.text)
-        if self.current.input.text != '':
-            values[1] = float(self.current.input.text)
-        if self.resistance.input.text != '':
-            values[2] = float(self.resistance.input.text)
-        if self.power.input.text != '':
-            values[3] = float(self.power.input.text)
-        v = values[0]
-        i = values[1]
-        r = values[2]
-        p = values[3]
+    def callback(self, instance) -> None:
+        # convert inputs to float numbers
+        v = float(self.voltage.input.text) if self.voltage.input.text else float(0)
+        i = float(self.current.input.text) if self.current.input.text else float(0)
+        r = float(self.resist.input.text) if self.resist.input.text else float(0)
+        p = float(self.power.input.text) if self.power.input.text else float(0)
 
-        if target == 'v':
-            if v != 0:
-                work = 'v = v'
-            elif i != 0:
-                if r != 0:
-                    v = i * r
-                    work = 'v = i*r'
-                elif p != 0:
-                    v = i / p
-                    work = 'v = i/p'
-            elif p != 0 and r != 0:
-                v = math.sqrt(p * r)
-                work = 'v = sqrt(p*r)'
+        # check if exactly 2 values are entered
+        num_zero = [value == 0 for value in (v, i, r, p)]
+        if sum(num_zero) != 2:
+            self.answer.text = "You should enter two values"
+            self.formula.text = ""
+            return
 
-            else:
-                v = 'Invalid Inputs!'
-            answer = f'Voltage is: {round(v, 4)} volts'
+        calc_dict = {
+            "vi": "(v, i, v / i, v * i)",
+            "vr": "(v, v / r, r, v * v / r)",
+            "vp": "(v, p / v, v * v / p, p)",
+            "ir": "(i * r, i, r, r * i * i)",
+            "ip": "(p / i, i, p / (i * i), p)",
+            "rp": "(math.sqrt(p * r), math.sqrt(p / r), r, p)",
+        }
 
-        elif target == 'i':
-            if i != 0:
-                work = 'i = i'
-            elif v != 0:
-                if r != 0:
-                    i = v / r
-                    work = 'i = v/r'
-                elif p != 0:
-                    i = p / v
-                    work = 'i = p/v'
-            elif p != 0 and r != 0:
-                i = math.sqrt(p) / r
-                work = 'i = sqrt(p)/r'
-            else:
-                i = 'Invalid Inputs!'
-            answer = f'Current is: {round(i, 4)} amps'
+        # find the couple of values we have
+        couple = ""
+        for index, value in enumerate(("v", "i", "r", "p")):
+            if not num_zero[index]:
+                couple += value
 
-        elif target == 'r':
-            if r != 0:
-                work = 'r = r'
-            elif i != 0:
-                if v != 0:
-                    r = v/i
-                    work = 'r = v/i'
-                elif p != 0:
-                    r = p/i**2
-                    work = 'r = p/i^2'
-            elif p != 0 and v != 0:
-                r = v**2/p
-                work = 'r = v^2/p'
-            else:
-                r = 'Invalid Inputs!'
-            answer = f'Resistance is: {round(r, 4)} ohms'
+        # do calculations
+        v, i, r, p = eval(calc_dict[couple])
+        self.formula.text = calc_dict[couple]
 
-        elif target == 'p':
-            if p != 0:
-                work = 'p = p'
-            elif v != 0:
-                if r != 0:
-                    p = v**2/r
-                    work = 'p = v^2/r'
-                elif i != 0:
-                    p = v*i
-                    work = 'p = v*i'
-            elif i != 0 and r != 0:
-                p = i**2 * r
-                work = 'p = i^2 * r'
-            else:
-                p = 'Invalid Inputs!'
-            answer = f'Power is: {round(p, 4)} watts'
-
-        self.answer.text = answer
-        self.formula.text = work
-
-
-class ButtonBox(BoxLayout):
-    def __init__(self, **kwargs):
-        super(ButtonBox, self).__init__(**kwargs)
-        self.cols = 4
-        self.rows = 1
-        self.solve_v = Button(text='Solve V', color=(1, 0, 0), bold=True)
-        self.solve_i = Button(text='Solve I', color=(0, 1, 0), bold=True)
-        self.solve_r = Button(text='Solve R', color=(0, 0.5, 1), bold=True)
-        self.solve_p = Button(text='Solve P', color=(1, 0, 1), bold=True)
-        self.add_widget(self.solve_v)
-        self.add_widget(self.solve_i)
-        self.add_widget(self.solve_r)
-        self.add_widget(self.solve_p)
+        # update answer text
+        self.answer.text = (
+            f"{str(round(v,4))} V == {str(round(i,4))} A == "
+            f"{str(round(r,4))} Ω == {str(round(p,4))} W"
+        )
 
 
 class LabelAndInput(BoxLayout):
@@ -147,9 +91,13 @@ class LabelAndInput(BoxLayout):
         super(LabelAndInput, self).__init__(**kwargs)
         self.cols = 2
         self.rows = 1
-        self.value = Label(text=input_text, color=input_color, bold=True, size_hint=(.4, 1))
+        self.value = Label(
+            text=input_text, color=input_color, bold=True, size_hint=(0.4, 1)
+        )
         self.add_widget(self.value)
-        self.input = TextInput(multiline=False, size_hint=(.6, 1))
+        self.input = TextInput(
+            multiline=False, size_hint=(0.6, 1), input_filter="float"
+        )
         self.add_widget(self.input)
 
 
@@ -158,5 +106,5 @@ class OhmsLawApp(App):
         return OhmsLawInputScreen()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     OhmsLawApp().run()
